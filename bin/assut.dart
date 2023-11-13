d7c72326d41ba3fd4b1be4441f1f5a3345c411ab
ICONS <om8859387@gmail.com>
2023-11-13 22:02:43 +0200

    first comet by intelij

---
+++ bin/assut.dart
@@ -0,0 +1,31 @@
+
+import 'dart:io';
+void main(){
+/*print ('enter your four nnumber');
+String? num1 = stdin.readLineSync();
+String? num2 = stdin.readLineSync();
+String? num3 = stdin.readLineSync();
+String? num4 = stdin.readLineSync();
+var result= int.parse(num1!) * int.parse(num2!)- (int.parse(num3!)*int.parse(num4!)) ;
+print ('rsult is $result' );
+print ('enter your numbers');
+String? num1 = stdin.readLineSync();
+String? num2 = stdin.readLineSync();
+ int num1int = int.parse(num1!) ;
+ int num2int = int.parse(num2!);
+ int resul = num1int%10;
+ int resul2 = num2int%10;
+ var finalres = resul + resul2;
+ print (finalres);*/
+print('enter radius');
+String? rad=stdin.readLineSync();
+double radnum=double.parse(rad!) ;
+ double moheet = 2 * 3.14 * radnum;
+ print(moheet);
+
+
+
+
+
+
+}
